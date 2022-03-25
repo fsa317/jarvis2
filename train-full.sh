5d3a5e388e4870ba9cd74d5f9bfb2b7266f1e5b8
MyHASS <fsa317@gmail.com>
2022-03-25 21:12:46 +0000

    updates

--- train-full.sh
+++ train-full.sh
@@ -3,5 +3,5 @@
 PRECISEDIR=../mycroft-precise
 
 
-precise-train -e 250 -s .5 $PRECISEDIR/hey-jarvis.net $PRECISEDIR/heyjarvis/
+precise-train -e 300 -s .2 $PRECISEDIR/hey-jarvis.net $PRECISEDIR/heyjarvis/
 
